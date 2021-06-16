c4f8903b8619ed7629bdc0179824ad8cfa067b32
Kazi Md. Mahidul Islam <mahidul.cse.du@gmail.com>
2021-06-17 02:22:21 +0600

    php from master

---
+++ phpfile.php
@@ -0,0 +1,19 @@
+<script>
+.h1
+{
+    background : black;
+}
+    
+</script>
+<!doctype html>
+<html>
+<head>
+    <title> hello this is php file!</title>
+</head>
+<body>
+    <div class = "h3">
+        <h3>Hei is this correct ! i am confused.</h3>
+    </div>
+
+</body>
+</html>
